0a580ea02e86c73c0de6c87b30bacdad93e6aa9b
William Jevne <envejw@gmail.com>
2018-07-28 00:55:41 -0700

    Change structure/naming to allow running tests automatically in VS Code

--- p1.py
+++ p1.py
@@ -1,9 +1,11 @@
 def sum_of_multiples_of_3_and_5(value):
-    sum = 0
+    res = 0
+
     for i in range(value):
         if i % 3 == 0 or i % 5 == 0:
-            sum += i     
-    return sum
+            res += i
+
+    return res
 
 if __name__ == "__main__":
     print(sum_of_multiples_of_3_and_5(1000))
--- tests/p1_test.py
+++ tests/p1_test.py
@@ -1,15 +1,19 @@
 import pytest
 
-from solution import sum_of_multiples_of_3_and_5
+from p1 import sum_of_multiples_of_3_and_5
+
 
 def test_1():
     assert sum_of_multiples_of_3_and_5(0) == 0
 
+
 def test_4():
     assert sum_of_multiples_of_3_and_5(4) == 3
 
+
 def test_6():
     assert sum_of_multiples_of_3_and_5(6) == 8
+
 
 def test_10():
     assert sum_of_multiples_of_3_and_5(10) == 23
